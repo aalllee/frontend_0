63a1d224fdebffa4dc21158b5586c368ac3163c1
ale2580 <graphicsprograming1@gmail.com>
2024-01-23 19:16:09 +0200

    fix menu icon toggle

--- scr.js
+++ scr.js
@@ -1,26 +1,24 @@
 val = document.getElementsByClassName("collapse_menu");
+var collapse_menu_icon = document.querySelector(".collapse_menu");
 
 val[0].addEventListener("click", function () {
-  console.log("test");
   var nav = document.getElementsByClassName("collapse_menu_content");
-
-  var style = getComputedStyle(nav[0]);
-
-  var collapse_menu_icon = document.querySelector(".collapse_menu");
   console.log(collapse_menu_icon);
   collapse_menu_icon.classList.toggle("header_icon_active");
-
   nav[0].style.display =
     getComputedStyle(nav[0]).display === "flex" ? "none" : "flex";
 });
 
 window.addEventListener("resize", function () {
-  // Update the display property based on the window width
   var nav = document.getElementsByClassName("collapse_menu_content");
 
-  if (window.innerWidth > 800) {
+  if (window.innerWidth > 750) {
     nav[0].style.display = "none";
-  } else {
-    // nav[0].style.display = "block";
+
+    var active_icon = document.querySelector(".header_icon_active");
+    if (active_icon !== null) {
+      console.log(active_icon);
+      active_icon.classList.toggle("header_icon_active");
+    }
   }
 });
