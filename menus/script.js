fbf00ac5c80e1ba3cb0f31da01b3e3f6bd53e404
ale2580 <graphicsprograming1@gmail.com>
2024-01-27 15:53:42 +0200

    added infinite side scroller

--- menus/script.js
+++ menus/script.js
@@ -16,3 +16,34 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+/*MENU 3########*/
+var btnForward = document.getElementById("forward-m3");
+var btnBack = document.getElementById("back-m3");
+console.log(btnForward);
+
+var scrollContainer = document.getElementsByClassName("scroll-outer-m3")[0];
+console.log(scrollContainer);
+
+btnForward.addEventListener("click", function () {
+  if (scrollContainer.scrollLeft === 700) {
+    scrollContainer.scrollLeft = 200;
+  }
+  scrollContainer.scrollTo({
+    top: 0,
+    left: scrollContainer.scrollLeft + 100,
+    behavior: "smooth",
+  });
+});
+
+btnBack.addEventListener("click", function () {
+  if (scrollContainer.scrollLeft === 0) {
+    scrollContainer.scrollLeft = 500;
+  }
+  scrollContainer.scrollTo({
+    top: 0,
+    left: scrollContainer.scrollLeft - 100,
+    behavior: "smooth",
+  });
+  console.log(scrollContainer.scrollLeft);
+});
